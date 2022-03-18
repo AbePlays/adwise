b11e4a64b04499ef844d9f905a32338c308cfa82
Abe <abhi.rawat456@gmail.com>
2022-03-18 11:01:37 +0530

    feat: UI done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import dice from "./assets/dice.svg";
+import pattern from "./assets/pattern.svg";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div class="bg-[#1f2632] min-h-screen flex justify-center items-center p-4">
+      <main class="bg-[#323a49] rounded-2xl text-[#cee3e9] p-8 text-center space-y-8 relative pb-16 max-w-lg mx-auto">
+        <h1 class="text-[#52ffa8] uppercase tracking-[4px] text-sm">
+          Advice #123
+        </h1>
+        <p class="font-bold text-2xl leading-8">
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
+        <img src={pattern} alt="" />
+        <button
+          class="bg-[#52ffa8] h-16 w-16 rounded-full absolute -bottom-8 left-0 right-0 mx-auto flex justify-center items-center"
+          aria-label="Generate Random Advice"
         >
-          Learn Solid
-        </a>
-      </header>
+          <img class="text-white" src={dice} alt="" />
+        </button>
+      </main>
     </div>
   );
 };
